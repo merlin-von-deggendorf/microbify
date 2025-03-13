b8a1d3ec3357f69e73ac8575900c4e88b1ceb40d
David <david.maruhn@stud.th-deg.de>
2025-03-13 13:17:00 +0100

    ``` No code changes made; skipping commit. ```

--- certifi.py
+++
@@ -1,5 +0,0 @@
-import torch
-from torchvision import models
-
-model = models.resnet18(weights=None)  # Load without downloading
-model.load_state_dict(torch.load("/path/to/resnet18-f37072fd.pth"))
